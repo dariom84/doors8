69c13cc347bfcfa74b9cfb20c6f1b451f4684ef6
Dario Gabriel Molina <dariom84@gmail.com>
2024-07-24 17:08:10 -0300

    probando search de nuevo

--- Contacts/Controls/beforeRender.js
+++ Contacts/Controls/beforeRender.js
@@ -4,7 +4,9 @@ debugger;
 
 var $d = $(document);
 
-var fld = await dSession.foldersGetFromId(1001).app.folders("/contacts_root/Contacts");
+dSession.foldersGetFromId(1003)
+
+var fld = await dSession.foldersGetFromId(1003);
 var res = await fld.search();
 
 console.log(res);
