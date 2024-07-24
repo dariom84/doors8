5873f4e7b4817c5680cf1d351fe68250f05d685e
Dario Gabriel Molina <dariom84@gmail.com>
2024-07-24 17:25:18 -0300

    probando search con filtro

--- Contacts/Controls/beforeRender.js
+++ Contacts/Controls/beforeRender.js
@@ -5,6 +5,11 @@ debugger;
 var $d = $(document);
 
 var fldAreas = await dSession.foldersGetFromId(1005);
-var res = await fldAreas.search();
+var res = await fldAreas.search({
+                                    fields: 'id, name',
+                                    formula: 'id <> 1',
+                                    order: 'id asc',
+                                    maxDocs: 0
+                                })
 
 console.log(res);
