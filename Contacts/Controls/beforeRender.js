ee5f414813729d95a0acb430c77e31be73e4800a
Dario Gabriel Molina <dariom84@gmail.com>
2024-07-25 11:02:15 -0300

    me atacho al evento

--- Contacts/Controls/beforeRender.js
+++ Contacts/Controls/beforeRender.js
@@ -1,10 +1,12 @@
 console.log("BeforeRender desde repo...");
 
+debugger;
+
+//Ejemplo include de un modulo de funciones
 var fn = await import(gitCdn({ owner: ctx.owner, repo: ctx.repo, path: '/Contacts/functions.mjs', fresh: true, url: true }));
 console.log(fn.saludar());
 
-debugger;
-
+//Ejemplo de un seach a la carpeta de areas por id
 var fldAreas = await dSession.foldersGetFromId(1008);
 var res = await fldAreas.search({
                                     fields: 'id, area',
@@ -15,10 +17,20 @@ var res = await fldAreas.search({
 
 console.log(res);
 
-
+//pagina/documento actual que se esta dibujando
 var $d = $(document);
 
+//Me subo al evento renderControl y pregunto por cada control que va dibujar
 $d.on('renderControl', (ev) => {
-    debugger;
     var ctx = ev.detail;
+    var ctlName = ctx.ctl.NAME;
+
+    switch (ctlName) {
+        case 'name':
+            debugger;
+            break;
+        case 'surname':
+            debugger;
+            break;
+    }
 });
