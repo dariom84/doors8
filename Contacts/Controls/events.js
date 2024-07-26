689e954672efbc90e014e1129799038173a467c7
Dario Gabriel Molina <dariom84@gmail.com>
2024-07-26 11:23:17 -0300

    acomodando gral

--- Contacts/Controls/events.js
+++ Contacts/Controls/events.js
@@ -1,20 +1,7 @@
 console.log("BeforeRender desde repo...");
 
-debugger;
-
 //Ejemplo include de un modulo de renderControls
 var rc = await import(gitCdn({ owner: ctx.owner, repo: ctx.repo, path: '/Contacts/renderControls.mjs', fresh: true, url: true }));
-
-//Ejemplo de un seach a la carpeta de areas por id
-var fldAreas = await dSession.foldersGetFromId(1008);
-var res = await fldAreas.search({
-                                    fields: 'id, area',
-                                    formula: 'id <> 1',
-                                    order: 'id asc',
-                                    maxDocs: 0
-                                })
-
-console.log(res);
 
 //pagina/documento actual que se esta dibujando 
 var $d = $(document);
