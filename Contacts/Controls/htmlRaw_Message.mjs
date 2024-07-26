05338f7160b6be5f3f6d0e773bdea5899ad7f6e3
Dario Gabriel Molina <dariom84@gmail.com>
2024-07-26 14:39:40 -0300

    probando search

--- Contacts/Controls/htmlRaw_Message.mjs
+++ Contacts/Controls/htmlRaw_Message.mjs
@@ -1,18 +1,23 @@
 export async function render(ctx){
-    let area = await getArea();
+    let area = await getArea(doc.fields('areaid').value);
 
     if (!doc.isNew) ctx.$this.append(`<h3>${doc.fields('surname').value}, ${doc.fields('NAME').value}, area: ${area}</h3>`);
 }
 
-async function getArea(){
-    //Ejemplo de un seach a la carpeta de areas por id
-    var fldAreas = await dSession.folders("/contacts_root/admin/area", 1001);
-    var res = await fldAreas.search({
-                                        fields: 'id, area',
-                                        formula: 'id <> 1',
-                                        order: 'id asc',
-                                        maxDocs: 0
-                                    })
+//Ejemplo de un seach a la carpeta de areas por id
+async function getArea(idArea){
+    let area = '';
 
-    return res[0].AREA;
+    if (idArea) {
+        var fldAreas = await dSession.folders("/contacts_root/admin/area", 1001);
+        var res = await fldAreas.search({
+                                            fields: 'id, area',
+                                            formula: `id = ${idArea}`,
+                                            order: 'id asc',
+                                            maxDocs: 0
+                                        })
+        area = res[0].AREA;
+    }
+
+    return area;
 }
