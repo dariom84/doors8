45013ca81f93f6adb89d03d3a53aaa97e8c1d415
Dario Gabriel Molina <dariom84@gmail.com>
2024-07-27 16:51:22 -0300

    reestruturación de carpetas

--- Contacts/Client/Lib/renderControls.mjs
+++ Contacts/Client/Lib/renderControls.mjs
@@ -1,15 +1,16 @@
-export async function renderControl(ctx, evnDetail){
-    var ctrlHtmlRawMessage = await import(gitCdn({ owner: ctx.owner, repo: ctx.repo, path: '/Contacts/Client/Controls/htmlRaw_Message.mjs', fresh: true, url: true }));
-    var ctrldateBirth = await import(gitCdn({ owner: ctx.owner, repo: ctx.repo, path: '/Contacts/Client/Controls/date_birth.mjs', fresh: true, url: true }));
+export async function renderControl(options){
+    var ctrlHtmlRawMessage = await import(gitCdn({ owner: options.ctx.owner, repo: options.ctx.repo, path: '/Contacts/Client/Controls/htmlRaw_Message.mjs', fresh: true, url: true }));
+    var ctrldateBirth = await import(gitCdn({ owner: options.ctx.owner, repo: options.ctx.repo, path: '/Contacts/Client/Controls/date_birth.mjs', fresh: true, url: true }));
 
-    var ctlName = evnDetail.ctl.NAME;
+    //var ctlName = evnDetail.ctl.NAME;
+    var ctlName = options.evDetail.ctl.NAME
 
     switch (ctlName) {
         case 'date_birth': 
-            ctrldateBirth.render(ctx, evnDetail);
+            ctrldateBirth.render(options);
             break;
         case 'htmlRaw_Message':
-            await ctrlHtmlRawMessage.render(evnDetail);
+            await ctrlHtmlRawMessage.render(options);
             break;
     }
 }
