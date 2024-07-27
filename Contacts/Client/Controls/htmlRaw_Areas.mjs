7dc13642b0971662d5ec6b9153d18af7b0599d98
Dario Gabriel Molina <dariom84@gmail.com>
2024-07-27 17:49:24 -0300

    Nuevo control htmlRaw_Areas

---
+++ Contacts/Client/Controls/htmlRaw_Areas.mjs
@@ -0,0 +1,23 @@
+export async function render(options){
+    //let area = await getArea(doc.fields('areaid').value);
+
+    if (!doc.isNew) options.evDetail.$this.append(`<h3>Listado de Areas</h3>`);
+}
+
+//Ejemplo de un seach a la carpeta de areas por id
+async function getArea(idArea){
+    let area = '';
+
+    if (idArea) {
+        var fldAreas = await dSession.folders("/contacts_root/admin/area", 1001);
+        var res = await fldAreas.search({
+                                            fields: 'id, area',
+                                            formula: `id = ${idArea}`,
+                                            order: 'id asc',
+                                            maxDocs: 0
+                                        })
+        area = res[0].AREA;
+    }
+
+    return area;
+}
--- Contacts/Client/Lib/renderControls.mjs
+++ Contacts/Client/Lib/renderControls.mjs
@@ -1,8 +1,8 @@
 export async function renderControl(options){
     var ctrlHtmlRawMessage = await import(gitCdn({ owner: options.ctx.owner, repo: options.ctx.repo, path: '/Contacts/Client/Controls/htmlRaw_Message.mjs', fresh: true, url: true }));
     var ctrldateBirth = await import(gitCdn({ owner: options.ctx.owner, repo: options.ctx.repo, path: '/Contacts/Client/Controls/date_birth.mjs', fresh: true, url: true }));
+    var ctrlhtmlRawAreas = await import(gitCdn({ owner: options.ctx.owner, repo: options.ctx.repo, path: '/Contacts/Client/Controls/htmlRaw_Areas.mjs', fresh: true, url: true }));
 
-    //var ctlName = evnDetail.ctl.NAME;
     var ctlName = options.evDetail.ctl.NAME
 
     switch (ctlName) {
@@ -11,6 +11,9 @@ export async function renderControl(options){
             break;
         case 'htmlRaw_Message':
             await ctrlHtmlRawMessage.render(options);
+            break;
+        case 'htmlRaw_Areas':
+            await ctrlhtmlRawAreas.render(options);
             break;
     }
 }
