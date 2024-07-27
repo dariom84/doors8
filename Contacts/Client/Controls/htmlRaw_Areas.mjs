fd4035114237318d2f4a8f1b74978648dd2e897e
Dario Gabriel Molina <dariom84@gmail.com>
2024-07-27 18:04:39 -0300

    probando tabla generica

--- Contacts/Client/Controls/htmlRaw_Areas.mjs
+++ Contacts/Client/Controls/htmlRaw_Areas.mjs
@@ -1,7 +1,7 @@
 export async function render(options){
     //let area = await getArea(doc.fields('areaid').value);
 
-    if (!doc.isNew) options.evDetail.$this.append(`<h3>Listado de Areas</h3>`);
+    if (!doc.isNew) options.evDetail.$this.append(`<h4>Listado de Areas</h4>`);
 }
 
 //Ejemplo de un seach a la carpeta de areas por id
@@ -20,4 +20,25 @@ async function getArea(idArea){
     }
 
     return area;
+}
+
+async function drawTable(opsSearch, opsTable){
+    let resSearch = await drawTableSearch(opsSearch, opsTable.path);
+
+    if (resSearch){
+        
+    }
+
+    return resSearch;
+}
+
+async function drawTableSearch(opsSearch, pathSearch){
+    var res = {};
+
+    if (pathFolder) {
+        var fld = await dSession.folders(pathSearch, 1001);
+        res = await fld.search(opsSearch);
+    }
+
+    return res;
 }
