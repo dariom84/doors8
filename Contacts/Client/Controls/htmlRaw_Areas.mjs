6700a1f774a88ac4170f9e779d1fe829ac5ef837
Dario Gabriel Molina <dariom84@gmail.com>
2024-07-27 18:10:15 -0300

    probando tabla generica

--- Contacts/Client/Controls/htmlRaw_Areas.mjs
+++ Contacts/Client/Controls/htmlRaw_Areas.mjs
@@ -1,35 +1,33 @@
 export async function render(options){
     //let area = await getArea(doc.fields('areaid').value);
+    debugger;
+
+    let opsSearch = {
+                        fields: 'id, area',
+                        formula: `id = ${idArea}`,
+                        order: 'id asc',
+                        maxDocs: 0
+                    }
+
+    let opsTable =  {
+                        path: '/contacts_root/admin/area',
+                        id: 'areas',
+                    }
+
+    let table = await drawTable(opsSearch, opsTable);
 
     if (!doc.isNew) options.evDetail.$this.append(`<h4>Listado de Areas</h4>`);
 }
 
-//Ejemplo de un seach a la carpeta de areas por id
-async function getArea(idArea){
-    let area = '';
-
-    if (idArea) {
-        var fldAreas = await dSession.folders("/contacts_root/admin/area", 1001);
-        var res = await fldAreas.search({
-                                            fields: 'id, area',
-                                            formula: `id = ${idArea}`,
-                                            order: 'id asc',
-                                            maxDocs: 0
-                                        })
-        area = res[0].AREA;
-    }
-
-    return area;
-}
-
 async function drawTable(opsSearch, opsTable){
+    let table = "<p>aca va mi tabla</>"
     let resSearch = await drawTableSearch(opsSearch, opsTable.path);
 
     if (resSearch){
-        
+
     }
 
-    return resSearch;
+    return table;
 }
 
 async function drawTableSearch(opsSearch, pathSearch){
