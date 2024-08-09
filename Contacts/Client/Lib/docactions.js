07d9e25cbf2f02d3ca035f847504f5befdb02c76
Dario Gabriel Molina <dariom84@gmail.com>
2024-08-09 09:01:11 -0300

    probando docactions

--- Contacts/Client/Lib/docactions.js
+++ Contacts/Client/Lib/docactions.js
@@ -1,5 +1,3 @@
 
-//desde consola del navegador para encodear una url: 
-    //encodeURIComponent("/jsactions.asp?owner=dariom84&repo=doors8&path=/Contacts/Client/Lib/docactions.js")
-    // [APPVIRTUALROOT]%2Fjsactions.asp%3Fowner%3Ddariom84%26repo%3Ddoors8%26path%3D%2FContacts%2FClient%2FLib%2Fdocactions.js
+// [APPVIRTUALROOT]/jsactions.asp?owner=dariom84&repo=doors8&path=/Contacts/Client/Lib/docactions.js&fresh=true
 debugger;
