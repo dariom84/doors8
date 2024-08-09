5248d8f0f746ccdfdd48152a9fded80d1dab68a6
Dario Gabriel Molina <dariom84@gmail.com>
2024-08-09 17:35:18 -0300

    nuevo control btnNode

--- Contacts/Client/Controls/btnNode.mjs
+++ Contacts/Client/Controls/btnNode.mjs
@@ -1,3 +1,11 @@
 export async function render(options){
     debugger;
+
+    let $btn = $('<button/>', {
+        class: 'btn btn-primary',
+    }).append('Node').appendTo(options.ctx.$this);
+
+    $btn.click(() => {
+        debuggerM
+    });
 }
