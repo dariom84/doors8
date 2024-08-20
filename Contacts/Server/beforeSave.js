ee5f053488645ed8d8a8c6c69e5eff7c4a49cb2e
Dario Gabriel Molina <dariom84@gmail.com>
2024-08-20 14:05:26 -0300

    cambios beforesave sever

--- Contacts/Server/beforeSave.js
+++ Contacts/Server/beforeSave.js
@@ -3,5 +3,5 @@ console.log("beforesave server");
 var wkfMod = await mlib.gitImport({ repo: 'Global', path: 'workflow.mjs' });
 wkfMod.setContext(ctx);
 
-//wkfMod.requeridos('surname');
-//wkfMod.raiseWkfErrors();
+wkfMod.requeridos('surname');
+wkfMod.raiseWkfErrors();
