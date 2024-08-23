91815df0774c016d96cd00ed1ab996d819f1260c
Dario Gabriel Molina <dariom84@gmail.com>
2024-08-23 15:25:27 -0300

    se creo nueva carpeta contact

--- Contacts/Contact/Server/beforeSave.js
+++ Contacts/Contact/Server/beforeSave.js
@@ -3,8 +3,8 @@
 console.log("beforesave server");
 
 //Para que funcione intelliSense, copiar el modulo en el proyecto pero no es que toma el import
-/** @type {import('../../_types/workflow.mjs')} */
-/** @type {import('../../_types/doorsapi2.mjs')} */
+/** @type {import('../../../_types/workflow.mjs')} */
+/** @type {import('../../../_types/doorsapi2.mjs')} */
 
 var wkfMod = await mlib.gitImport({ repo: 'Global', path: 'workflow.mjs' });
 wkfMod.setContext(ctx);
