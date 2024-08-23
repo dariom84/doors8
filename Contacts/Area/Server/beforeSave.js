15f4f3af07e5aa02b53ba77b8d16f0cd8b25e058
Dario Gabriel Molina <dariom84@gmail.com>
2024-08-23 16:00:16 -0300

    beforeSave Area

---
+++ Contacts/Area/Server/beforeSave.js
@@ -0,0 +1,9 @@
+//Para que funcione intelliSense, copiar el modulo en el proyecto pero no es que toma el import
+/** @type {import('../../../_types/workflow.mjs')} */
+/** @type {import('../../../_types/doorsapi2.mjs')} */
+
+var wkfMod = await mlib.gitImport({ repo: 'Global', path: 'workflow.mjs' });
+wkfMod.setContext(ctx);
+
+wkfMod.requeridos('area');
+wkfMod.raiseWkfErrors();
